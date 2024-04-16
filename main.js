3719168c5f151285cb30a06ea43f87c4477b5b2b
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-04-16 19:32:32 -0300

    16/04

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 //Laço de repetição
 
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j = 0; j < botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -18,21 +18,29 @@ for(let i = 0; i < botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date(); //Retorna a data atual do computador
-const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
+const tempoObjetivo2 = new Date("2024-07-07T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-13T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-//transformação do tempo de milessegundos para "números redondos"
-let tempoFinal = tempoObjetivo1 - tempoAtual;
 
-let segundos = Math.floor(tempoFinal / 1000); //O Math.floor permite que os números sejam arredondados após as divisões
-let minutos = Math.floor (segundos / 60);     // o sinal % nos retorna o resto da divisão
-let horas = Math.floor (minutos / 60);
-let dias = Math.floor(horas / 24);
+function calculaTempo(tempoObjetivo) {
+    let tempoFinal = tempoObjetivo - tempoAtual;
+                                                //transformação do tempo de milessegundos para "números redondos"
+    let segundos = Math.floor(tempoFinal / 1000); //O Math.floor permite que os números sejam arredondados após as divisões
+    let minutos = Math.floor(segundos / 60);     // o sinal % nos retorna o resto da divisão
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-segundos %= 60; //para saber qual é o resto da divisão
-minutos %= 60;  
-horas %= 24;
+    segundos %= 60; //para saber qual é o resto da divisão
+    minutos %= 60;
+    horas %= 24;
 
-//para fazer a exibição do tempo usando "concatenação" (coloca as coisas juntas usando o sinal de +)
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
- 
+    //Para mostrar as informações na tela
+    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+}
 
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
